e89993ba5df761c29b5480d685ccbe8f781061df
Viraz Malhotra <virajm72@gmail.com>
2021-01-23 16:53:14 +0530

    refactor: margin pool improvements for phase 1

--- packages/hardhat/contracts/MarginPool.sol
+++ packages/hardhat/contracts/MarginPool.sol
@@ -10,6 +10,8 @@ contract MarginPool {
     using SafeERC20 for IERC20;
 
     address public creditPool;
+    uint256 public minSolvencyRatio;
+    uint256 public totalBorrowedAmount;
 
     struct Borrower {
         uint256 duration;
@@ -24,8 +26,9 @@ contract MarginPool {
     ILendingPool constant lendingPool = ILendingPool(address(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9)); // on mainnet
     IProtocolDataProvider constant dataProvider = IProtocolDataProvider(address(0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d)); //on mainnet
 
-    constructor(address _creditPool) public {
+    constructor(address _creditPool, uint256 _minSolvencyRatio) public {
         creditPool = _creditPool;
+        minSolvencyRatio = _minSolvencyRatio;
     }
     // to be called by a borrower with the duration in seconds and amount they wish to borrow
     // margin amount will be 10 % of _amount calculated on front end to avoid soldity decimal issue
@@ -33,21 +36,28 @@ contract MarginPool {
     // _asset is the stable coin they wish to borrow
     // borrow for a particular borrower will be allowed once for a particular borrower
     function invest(uint256 _amount, address _asset, address _vault, uint256 _marginAmount, uint256 _duration) public {
+      // get atoken address
       (address _debtToken, ,) =  dataProvider.getReserveTokensAddresses(_asset);
-
-      if (IERC20(_asset).balanceOf(address(this)) > 0) {
-          require(IERC20(_asset).balanceOf(address(this)) >  _amount, "MarginPool: insufficient debt balance");
-      } else {
-          uint256 _delegatedAmount = IStableDebtToken(_debtToken).borrowAllowance(creditPool, address(this));
-          require(_delegatedAmount >= _amount, "MarginPool: insufficient debt allowance");
-      }  
-          uint256 solvencyRatio = _amount.add(_marginAmount).div(_amount);
-          uint256 duration = block.timestamp.add(_duration);
-          Borrower memory borrower = delegateeDeposits[msg.sender];
-          require(!borrower.hasBorrowed, "MarginPool: borrower has borrowed previously");
-          delegateeDeposits[msg.sender] = Borrower(duration, _marginAmount, _amount.add(_marginAmount), solvencyRatio, true);
-          lendingPool.borrow(_asset, _amount, 1, 0, creditPool);
-          YearnVault(_vault).deposit(_amount.add(_marginAmount));
+      // get the borrowing allowance
+      uint256 _delegatedAmount = IStableDebtToken(_debtToken).borrowAllowance(creditPool, address(this));
+      require(_delegatedAmount >= totalBorrowedAmount, "MarginPool: insufficient debt allowance");
+      // compute the solvency ratio added this buffere 10^5 since solidity does not handle decimal places
+      // so to render solvency ratio on ui we need to divide by 10^5 in the js code 
+      uint256 solvencyRatio = (_amount.mul(100000)).add(_marginAmount).div(_amount);
+      require(solvencyRatio >= minSolvencyRatio.mul(100000), "MarginPool: insufficient margin amount");
+      // getting the actual timestamp for the duration
+      uint256 duration = block.timestamp.add(_duration);
+      // checking if the borrower already exists
+      Borrower memory borrower = delegateeDeposits[msg.sender];
+      require(!borrower.hasBorrowed, "MarginPool: borrower has borrowed previously");
+      // strong borrowe details in storage
+      delegateeDeposits[msg.sender] = Borrower(duration, _marginAmount, _amount.add(_marginAmount), solvencyRatio, true);
+      // setting total borrowed amount in storage
+      totalBorrowedAmount += _amount;
+      // getting the delgated credit amount from aave
+      lendingPool.borrow(_asset, _amount, 1, 0, creditPool);
+      // investing to yearn vault
+      YearnVault(_vault).deposit(_amount.add(_marginAmount));
     }
     // to be called by borrower to repay the borrowed
     //NOTE - this does not include reward distribution currently
@@ -63,7 +73,9 @@ contract MarginPool {
         borrower.investment = 0;
         borrower.solvencyRatio = 0;
         borrower.hasBorrowed = false;
+        // withdrawing yield amount from yearn
         YearnVault(_vault).withdraw(investment);
+        // repaying credit  pool the borrowed funds
         lendingPool.repay(_asset, investment.sub(margin), 1, creditPool);
     }
 
